fd185e088904586ef960abc3968795ea910ee48e
Pedro Bim <pedrodinizz2@outlook.com>
2023-07-31 18:19:40 -0300

    Move games on list

--- src/main/java/io/github/flea777/dslist/controllers/GameListController.java
+++ src/main/java/io/github/flea777/dslist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package io.github.flea777.dslist.controllers;
 
 import io.github.flea777.dslist.dto.GameListDTO;
 import io.github.flea777.dslist.dto.GameMinDTO;
+import io.github.flea777.dslist.dto.ReplacementDTO;
 import io.github.flea777.dslist.services.GameListService;
 import io.github.flea777.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +30,10 @@ public class GameListController {
     public List<GameMinDTO> findByList(@PathVariable Long listId) {
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
+    }
+
+    @PostMapping (value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
